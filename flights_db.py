7bf2bf92f38bcecfbac34c8ddee2c7f21ee2c7ed
hoopsbwc34 <github.fsn@neverbox.com>
2019-05-28 22:15:34 -0600

    Adding mysql support

---
+++ flights_db.py
@@ -0,0 +1,15 @@
+import configparser
+import mysql.connector as mysql
+
+config = configparser.ConfigParser()
+config.read('/home/matt/SWA-Checkin/config.ini')
+
+def connect():
+    db = mysql.connect(
+        host = config['mysqlDB']['host'],
+        user = config['mysqlDB']['user'],
+        passwd = config['mysqlDB']['pass'],
+        database = config['mysqlDB']['db']
+    )
+
+    return db
