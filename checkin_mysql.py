4c28a9cde642b02dee83296432e73f49d5b1ba07
hoopsbwc34 <github.fsn@neverbox.com>
2019-07-19 16:32:33 -0600

    fixing threading issues

--- checkin_mysql.py
+++ checkin_mysql.py
@@ -17,10 +17,6 @@ def schedule_checkin(flight_time, reservation):
     checkin_time = flight_time - timedelta(days=1)
     current_time = datetime.utcnow().replace(tzinfo=utc)
 
-    # open database to store boarding number
-    db = flights_db.connect()
-    cursor = db.cursor(dictionary=True)
-
     # check to see if we need to sleep until 24 hours before flight
     if checkin_time > current_time:
         # calculate duration to sleep
@@ -36,19 +32,19 @@ def schedule_checkin(flight_time, reservation):
             print("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
             boarding_num = "%s%s" % (doc['boardingGroup'],doc['boardingPosition'])
 
+            # open database to store boarding number
+            db = flights_db.connect()
+            cursor = db.cursor(dictionary=True)
+
             #checked in so don't try next time
             query = "UPDATE flightinfo SET boardingnum=%s WHERE conf=%s"
             cursor.execute(query,(boarding_num, doc['confirmationNumber']))
             db.commit()
-    db.close()
+            db.close()
 
 def set_takeoff(reservation_number, first_name, last_name, notify=[]):
     r = Reservation(reservation_number, first_name, last_name, notify)
     body = r.lookup_existing_reservation()
-
-    # Get our local current time
-    now = datetime.utcnow().replace(tzinfo=utc)
-    tomorrow = now + timedelta(days=1)
 
     # find all eligible legs for checkin
     for leg in body['bounds']:
@@ -56,9 +52,11 @@ def set_takeoff(reservation_number, first_name, last_name, notify=[]):
         print("calc departure in leg")
         airport = "{}, {}".format(leg['departureAirport']['name'], leg['departureAirport']['state'])
         takeoff = "{} {}".format(leg['departureDate'], leg['departureTime'])
+        for item in leg['flights']:
+            flightnum = item['number']
         airport_tz = openflights.timezone_for_airport(leg['departureAirport']['code'])
         date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
-        return date
+    return (date, flightnum)
 
 def auto_checkin(threads, reservation_number, first_name, last_name, notify=[]):
     r = Reservation(reservation_number, first_name, last_name, notify)
@@ -91,24 +89,29 @@ if __name__ == '__main__':
 
     print("======================")
     print("Starting Checkin Mysql")
+    print(str(datetime.now()))
     db = flights_db.connect()
     cursor = db.cursor(dictionary=True)
 
     # capture takeoff times if not set in mysql
-    query = "SELECT * FROM flightinfo WHERE takeoff IS NULL"
+    query = "SELECT * FROM flightinfo WHERE takeoff IS NULL and conf IS NOT NULL"
 
     cursor.execute(query)
 
     records = cursor.fetchall()
 
+    flight_details = []
+
     for record in records:
         print("Getting Takeoff time for {}".format(record['conf']))
-        takeoff_time = set_takeoff(record['conf'], record['first'], record['last'])
+        flight_details = set_takeoff(record['conf'], record['first'], record['last'])
         confnum=record['conf']
 
-        query = "UPDATE flightinfo SET takeoff=%s WHERE conf=%s"
-        print(query,takeoff_time,confnum)
-        cursor.execute(query,(takeoff_time,confnum))
+        takeoff_time = flight_details[0]
+        flightnum = flight_details[1]
+    
+        query = "UPDATE flightinfo SET takeoff=%s, flightnum=%s WHERE conf=%s"
+        cursor.execute(query,(takeoff_time,flightnum,confnum))
         db.commit()
 
     # get those that are less than two hours from checkin (run cron every two hours)
@@ -117,6 +120,7 @@ if __name__ == '__main__':
     cursor.execute(query)
 
     records = cursor.fetchall()
+    db.close()
     threads = []
 
     for record in records:
@@ -137,7 +141,6 @@ if __name__ == '__main__':
             notifications.append({'mediaType': 'SMS', 'phoneNumber': mobile})
 
         threads = auto_checkin(threads,reservation_number, first_name, last_name, notifications)
-    db.close()
     try:
         # cleanup threads while handling Ctrl+C
         while True:
