acf06fa5f14ad94642e4617bc6ca554efaa55923
hoopsbwc34 <github.fsn@neverbox.com>
2020-01-25 15:20:30 -0700

    added primary key, fixed timing, moved to python3

--- checkin_mysql.py
+++ checkin_mysql.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from datetime import datetime
 from datetime import timedelta
 from dateutil.parser import parse
@@ -13,7 +13,7 @@ import flights_db
 
 CHECKIN_EARLY_SECONDS = 5
 
-def schedule_checkin(flight_time, reservation):
+def schedule_checkin(flight_time, reservation, prikey):
     checkin_time = flight_time - timedelta(days=1)
     current_time = datetime.utcnow().replace(tzinfo=utc)
 
@@ -24,12 +24,12 @@ def schedule_checkin(flight_time, reservation):
         # pretty print our wait time
         m, s = divmod(delta, 60)
         h, m = divmod(m, 60)
-        print("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s))
+        print(("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s)))
         time.sleep(delta)
     data = reservation.checkin()
     for flight in data['flights']:
         for doc in flight['passengers']:
-            print("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
+            print(("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition'])))
             boarding_num = "%s%s" % (doc['boardingGroup'],doc['boardingPosition'])
 
             # open database to store boarding number
@@ -37,8 +37,8 @@ def schedule_checkin(flight_time, reservation):
             cursor = db.cursor(dictionary=True)
 
             #checked in so don't try next time
-            query = "UPDATE flightinfo SET boardingnum=%s WHERE conf=%s"
-            cursor.execute(query,(boarding_num, doc['confirmationNumber']))
+            query = "UPDATE flightinfo SET boardingnum=%s WHERE conf=%s AND prikey=%s"
+            cursor.execute(query,(boarding_num, doc['confirmationNumber'], prikey))
             db.commit()
             db.close()
 
@@ -60,21 +60,26 @@ def set_takeoff(reservation_number, first_name, last_name, notify=[]):
         for item in leg['flights']:
             flightnum = item['number']
         airport_tz = openflights.timezone_for_airport(leg['departureAirport']['code'])
-        date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
+          
+        # need to convert this to computer time so we start checkin at the right time
+        cpu_tz = openflights.timezone_for_airport('DEN')
+        temp = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
+        date = temp.astimezone(cpu_tz)
+
         if i is 0:
             query = "UPDATE flightinfo SET takeoff=%s, flightnum=%s WHERE conf=%s"
-            cursor.execute(query,(takeoff,flightnum,reservation_number))
+            cursor.execute(query,(date,flightnum,reservation_number))
             db.commit()
         else:
             query = "INSERT INTO flightinfo (takeoff, flightnum, conf, first, last) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(query,(takeoff,flightnum,reservation_number, first_name, last_name))
+            cursor.execute(query,(date,flightnum,reservation_number, first_name, last_name))
             db.commit()
         i+=1 
 
     db.close()
     return ()
 
-def auto_checkin(threads, reservation_number, first_name, last_name, notify=[]):
+def auto_checkin(threads, reservation_number, first_name, last_name, prikey, notify=[]):
     r = Reservation(reservation_number, first_name, last_name, notify)
     body = r.lookup_existing_reservation()
 
@@ -90,11 +95,13 @@ def auto_checkin(threads, reservation_number, first_name, last_name, notify=[]):
         takeoff = "{} {}".format(leg['departureDate'], leg['departureTime'])
         airport_tz = openflights.timezone_for_airport(leg['departureAirport']['code'])
         date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
+        print(date)
+        print(now)
         if date > now:
             # found a flight for checkin!
-            print("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p')))
+            print(("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p'))))
             # Checkin with a thread
-            t = Thread(target=schedule_checkin, args=(date, r))
+            t = Thread(target=schedule_checkin, args=(date, r, prikey))
             t.daemon = True
             t.start()
             threads.append(t)
@@ -105,7 +112,7 @@ if __name__ == '__main__':
 
     print("======================")
     print("Starting Checkin Mysql")
-    print(str(datetime.now()))
+    print((str(datetime.now())))
     db = flights_db.connect()
     cursor = db.cursor(dictionary=True)
 
@@ -117,7 +124,7 @@ if __name__ == '__main__':
     records = cursor.fetchall()
 
     for record in records:
-        print("Getting Takeoff time for {}".format(record['conf']))
+        print(("Getting Takeoff time for {}".format(record['conf'])))
         set_takeoff(record['conf'], record['first'], record['last'])
 
     # get those that are less than two hours from checkin (run cron every two hours)
@@ -130,12 +137,13 @@ if __name__ == '__main__':
     threads = []
 
     for record in records:
-        print("Checkin time for {}".format(record['conf']))
+        print(("Checkin time for {}".format(record['conf'])))
         reservation_number = record['conf']
         first_name = record['first']
         last_name = record['last']
         email = record['email']
         mobile = record['mobile']
+        prikey = record['prikey']
 
         # build out notifications
         notifications = []
@@ -146,7 +154,7 @@ if __name__ == '__main__':
             print("adding mobile")
             notifications.append({'mediaType': 'SMS', 'phoneNumber': mobile})
 
-        threads = auto_checkin(threads,reservation_number, first_name, last_name, notifications)
+        threads = auto_checkin(threads,reservation_number, first_name, last_name, prikey, notifications)
     try:
         # cleanup threads while handling Ctrl+C
         while True:
